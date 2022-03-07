3d52f36a71485efa17f6ed0c04eaa840ac092a40
welcome <welcome@Dell>
2022-03-07 17:55:25 +0530

    First Commit

---
+++ Day2_OOPs/src/com/tns/pack1/Pack11.java
@@ -0,0 +1,12 @@
+package com.tns.pack1;
+
+public class Pack11
+{
+	public void message()
+	{
+		System.out.println("Welcome To C2TC");
+	
+
+	}
+
+}
---
+++ Day2_OOPs/src/com/tns/pack2/Pack12.java
@@ -0,0 +1,12 @@
+package com.tns.pack2;
+import com.tns.pack1.*;
+public class Pack12 {
+
+	public static void main(String[] args)
+	{
+		Pack11 obj=new Pack11();
+		obj.message();
+
+	}
+
+}
