c90cc3a8e30b354691a619b88426a0af358a906c
welcome <welcome@Dell>
2022-04-01 14:19:33 +0530

    First Commit

--- Day7 CRUD/src/com/tns/dbcrud/CrudDb.java
+++ Day7 CRUD/src/com/tns/dbcrud/CrudDb.java
@@ -2,9 +2,7 @@ package com.tns.dbcrud;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 
 public class CrudDb {
