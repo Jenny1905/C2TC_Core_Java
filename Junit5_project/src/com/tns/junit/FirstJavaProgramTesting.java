ab82b7715591c946a45dae9b76e1329d9a4356c5
welcome <welcome@Dell>
2022-04-08 14:08:45 +0530

    First Commit

---
+++ Junit5_project/src/com/tns/junit/FirstJavaProgramTesting.java
@@ -0,0 +1,17 @@
+package com.tns.junit;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstJavaProgramTesting 
+{
+	@Test
+	void display()
+	{
+		System.out.println("Welcome to C2TC");
+	}
+	@Test
+	void print()
+	{
+		System.out.println("Welcome to Core Java");
+	}
+}
