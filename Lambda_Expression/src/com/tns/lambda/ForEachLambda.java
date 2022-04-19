85f542b875e4d65ed2effa394db7baf2157b4dbb
welcome <welcome@Dell>
2022-04-19 12:24:13 +0530

    First Commit

---
+++ Lambda_Expression/src/com/tns/lambda/ForEachLambda.java
@@ -0,0 +1,17 @@
+package com.tns.lambda;
+import java.util.*;  
+public class ForEachLambda{  
+    public static void main(String[] args) {  
+          
+        List<String> list=new ArrayList<String>();  
+        list.add("ankit");  
+        list.add("mayank");  
+        list.add("irfan");  
+        list.add("jai");  
+          
+        list.forEach(  
+            (n)->System.out.println(n)  
+        );  
+    }  
+}  
+
---
+++ Lambda_Expression/src/com/tns/lambda/Lambda4.java
@@ -0,0 +1,35 @@
+package com.tns.lambda;
+
+//Without Lambda Expression
+/*interface Drawable{  
+    public void draw();  
+}  
+public class  Lambda4  {  
+    public static void main(String[] args) {  
+        int width=10;  
+  
+        //without lambda, Drawable implementation using anonymous class  
+        Drawable d=new Drawable(){  
+            public void draw(){System.out.println("Drawing "+width);}  
+        };  
+        d.draw();  
+    }  
+}  */
+
+//with Lambda Expression
+@FunctionalInterface  //It is optional  
+interface Drawable{  
+    public void draw();  
+}  
+  
+public class Lambda4 {  
+    public static void main(String[] args) {  
+        int width=10;  
+          
+        //with lambda  
+        Drawable d2=()->{  
+            System.out.println("Drawing "+width);  
+        };  
+        d2.draw();  
+    }  
+}  
